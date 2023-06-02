45e8df684192823ff5300811cb5ad8216178984a
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-06-02 01:52:42 -0700

    customized admin panel

--- eduboost/admin.py
+++ eduboost/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Course, Lesson, Video
 # Register your models here.
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    exclude = ('students',)
+admin.site.register(Course, CourseAdmin)
+
+
+class VideoInline(admin.TabularInline):
+    model = Video
+    extra = 1
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [VideoInline]
+
+
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Video)
-admin.site.register(Lesson)
--- eduboost/models.py
+++ eduboost/models.py
@@ -8,7 +8,7 @@ def get_default_image():
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, default='course')
     description = models.TextField()
     students = models.ManyToManyField(User, related_name='courses')
     preview_img = models.ImageField(upload_to='images/', default=get_default_image)
@@ -19,8 +19,13 @@ class Course(models.Model):
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, default='lesson')
+    def __str__(self):
+        return self.title
 
 class Video(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='videos')
     youtube_link = models.URLField()
+    title = models.CharField(max_length=100, default='video')
+    def __str__(self):
+        return self.title
