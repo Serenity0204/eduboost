251bd8cff36cebad91e8f970f6787da43d2479b0
serenity0204 <yuhenglin0204@gmail.com>
2023-06-07 15:16:01 -0700

    changed requirements.txt

--- eduboost/views.py
+++ eduboost/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.contrib import messages
 from .models import Course
 
@@ -10,46 +11,51 @@ def home_view(request):
     ## add popular courses later
     courses = Course.objects.all()
     popular_courses = Course.objects.filter(is_featured=True)
-    context = {'courses': courses, 'popular_courses': popular_courses}
-    return render(request, 'home.html', context)
+    context = {"courses": courses, "popular_courses": popular_courses}
+    return render(request, "home.html", context)
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         # login if anthenticate success
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            message = 'Incorrect Username or Password Entered'
+            message = "Incorrect Username or Password Entered"
             messages.error(request, message)
-            return render(request, 'login.html')
+            return render(request, "login.html")
     else:
-        return render(request, 'login.html')
+        return render(request, "login.html")
+
 
 def register_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
         # throw error when user exists
         try:
-            user = User.objects.create_user(username=username, password=password, email=email)
+            user = User.objects.create_user(
+                username=username, password=password, email=email
+            )
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         except IntegrityError:
-            messages.error(request, 'Username Already Exists')
-            return redirect('register')
+            messages.error(request, "Username Already Exists")
+            return redirect("register")
     else:
-        return render(request, 'register.html')
+        return render(request, "register.html")
+
 
 def logout_view(request):
     logout(request)
-    messages.success(request, 'User Logged Out')
-    return redirect('login')
+    messages.success(request, "User Logged Out")
+    return redirect("login")
+
 
 def my_courses_view(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
