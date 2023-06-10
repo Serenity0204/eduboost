af86dd672158a490a5f3eef17ad2f892f6583745
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-06-10 10:56:30 -0700

    added more settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+## comment out staticfiles_dirs for deployment, and uncomment static_root
+# STATIC_ROOT = os.path.join(BASE_DIR, "static") 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
