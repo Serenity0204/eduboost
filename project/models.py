319df4f926713b5b6f69c82f89ef6914662e4496
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-08-17 00:11:33 -0700

    added project github url

--- project/models.py
+++ project/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)  # New field for GitHub URL
 
     def __str__(self):
         return self.title
